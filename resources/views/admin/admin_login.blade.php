347ab16b799077e0434fe3056061c24fdd528fd7
dev063 <ram.kumar@onsumaye.com>
2021-03-09 14:39:31 +0530

    dashboard update

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -25,6 +25,23 @@
         <!-- Content Wrapper -->
         <div class="login-wrapper"> 
             <div class="container-center">
+                @if(Session::has('flash_message_error'))
+                <div class="alert alert-sm alert-danger alert-block" role="alert">
+                   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                   </button>
+                   <strong>{!! session('flash_message_error') !!}</strong>
+                </div>
+                @endif
+                
+                @if(Session::has('flash_message_success'))
+                <div class="alert alert-sm alert-success alert-block" role="alert">
+                   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                   </button>
+                   <strong>{!! session('flash_message_success') !!}</strong>
+                </div>
+                @endif
             <div class="login-area">
                 <div class="panel panel-bd panel-custom">
                     <div class="panel-heading">
