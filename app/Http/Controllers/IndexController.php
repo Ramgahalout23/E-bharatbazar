318f0eae757fc61da680edc019cc6acd563fdf4d
dev063 <ram.kumar@onsumaye.com>
2021-03-20 17:18:23 +0530

    code update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Banners;
+use App\Category;
+use App\Products;
 class IndexController extends Controller
 {
     function index(){
-        
-        return view('Ebharatbazar/index');
+        $banners = Banners::where('status','1')->orderby('sort_order','asc')->get();
+        $categories = Category::with('categories')->where(['parent_id'=>0])->get();
+        $products = Products::paginate(3);
+        return view('Ebharatbazar/index')->with(compact('banners','categories','products'));
+
     }
 }
