eb73b373f79a955ad3bf297094919bbf4393a3ec
Ramgahalout23 <ramkumargahalout67@gmail.com>
2021-04-16 04:29:47 +0530

    set pagination

--- resources/views/Ebharatbazar/index.blade.php
+++ resources/views/Ebharatbazar/index.blade.php
@@ -30,7 +30,7 @@
     <!-- Start Shop Page  -->
     <div class="shop-box-inner">
         <div class="container">
-            <div class="row">
+        <div class="row">
             <div class="col-xl-3 col-lg-3 col-sm-12 col-xs-12 sidebar-shop-left">
                     <div class="product-categori">
                         <div class="search-product">
@@ -59,9 +59,94 @@
                                 @endforeach
                             </div>
                         </div>
+                        <div class="filter-price-left">
+                            <div class="title-left">
+                                <h3>Price</h3>
+                            </div>
+                            <div class="price-box-slider">
+                                <div id="slider-range"></div>
+                                <p>
+                                    <input type="text" id="amount" readonly style="border:0; color:#fbb714; font-weight:bold;">
+                                    <button class="btn hvr-hover" type="submit">Filter</button>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="filter-brand-left">
+                            <div class="title-left">
+                                <h3>Brand</h3>
+                            </div>
+                            <div class="brand-box">
+                                <ul>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios1" value="Yes" type="radio">
+                                            <label for="Radios1"> Supreme </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios2" value="No" type="radio">
+                                            <label for="Radios2"> A Bathing Ape </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios3" value="declater" type="radio">
+                                            <label for="Radios3"> The Hundreds </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios4" value="declater" type="radio">
+                                            <label for="Radios4"> Alife </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios5" value="declater" type="radio">
+                                            <label for="Radios5"> Neighborhood </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios6" value="declater" type="radio">
+                                            <label for="Radios6"> CLOT </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios7" value="declater" type="radio">
+                                            <label for="Radios7"> Acapulco Gold </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios8" value="declater" type="radio">
+                                            <label for="Radios8"> UNDFTD </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios9" value="declater" type="radio">
+                                            <label for="Radios9"> Mighty Healthy </label>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="radio radio-danger">
+                                            <input name="survey" id="Radios10" value="declater" type="radio">
+                                            <label for="Radios10"> Fiberops </label>
+                                        </div>
+                                    </li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+
+                        
 
                     </div>
                 </div>
+                
                 <div class="col-xl-9 col-lg-9 col-sm-12 col-xs-12 shop-content-right">
                     <div class="right-product-box">
                         <div class="product-item-filter row">
